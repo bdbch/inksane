2ffc339d92b8b9bcd3a975e606ce65320f82a378
bdbch <git@bdbch.com>
2026-07-30 19:55:13 +0200

    feat(core): enhance Editor class with options property and content accessors

--- packages/core/src/editor/Editor.ts
+++ packages/core/src/editor/Editor.ts
@@ -4,12 +4,14 @@ import { EventEmitter } from "./EventEmitter.ts";
 import type { EditorEvents, EditorOptions } from "./types.ts";
 
 export class Editor extends EventEmitter<EditorEvents> {
+  options: EditorOptions;
   state: EditorState;
   view: EditorView;
 
   constructor(options: EditorOptions) {
     super();
-    const { element, content } = options;
+    this.options = options;
+    const { element, content } = this.options;
 
     this.emit("beforeCreate", { editor: this });
 
@@ -21,11 +23,22 @@ export class Editor extends EventEmitter<EditorEvents> {
       parent: element,
     });
 
+    // @ts-expect-error we set the editor here so users can access the editor object from the DOM element
+    this.options.element["editor"] = this;
+
     this.emit("create", { editor: this });
   }
 
   // TODO: extend destruction of editor instance here
   public destroy(): void {
     this.view.destroy();
+  }
+
+  get content(): string {
+    return this.state.doc.toString();
+  }
+
+  get json(): string[] {
+    return this.state.doc.toJSON();
   }
 }
